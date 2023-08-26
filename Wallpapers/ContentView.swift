27a9da2bf12dc7c6fe484c0d8e1cc20d56ee4d06
gaozimeng <gaozimeng0425@gmail.com>
2023-08-26 13:13:50 +0800

    Initial Commit

---
+++ Wallpapers/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Wallpapers
+//
+//  Created by 高子蒙 on 2023/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wallpapers/WallpapersApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallpapersApp.swift
+//  Wallpapers
+//
+//  Created by 高子蒙 on 2023/8/26.
+//
+
+import SwiftUI
+
+@main
+struct WallpapersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
